8775ede76dba6f214bc2ba3fbf6a382d40232a54
yujaemin <youjmen15@gmail.com>
2021-04-04 21:20:59 +0900

    [ADD] retrofit, sharedpreference setting

---
+++ DMtime/app/src/main/java/com/jaemin/dmtime/AuthorizationInterceptor.kt
@@ -0,0 +1,16 @@
+package com.jaemin.dmtime
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthorizationInterceptor(private val preference : SharedPreferencesManager) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request()
+                .newBuilder()
+                .addHeader("Authorization",preference.getInfo("accessToken"))
+                .build()
+
+            return chain.proceed(request)
+    }
+
+}
---
+++ DMtime/app/src/main/java/com/jaemin/dmtime/di/MainApplication.kt
@@ -0,0 +1,19 @@
+package com.jaemin.dmtime.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication() : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MainApplication)
+            modules(
+                listOf(
+                    networkModule,
+                )
+            )
+        }
+    }
+}
