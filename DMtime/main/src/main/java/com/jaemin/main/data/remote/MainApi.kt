1b96b5cd5f8b001e358c068a75b5c1a401a0f16e
yujaemin <youjmen15@gmail.com>
2021-04-08 20:00:23 +0900

    [ADD] 메인 페이지 api 추가

--- DMtime/main/src/main/java/com/jaemin/main/data/remote/MainApi.kt
+++ DMtime/main/src/main/java/com/jaemin/main/data/remote/MainApi.kt
@@ -1,4 +1,20 @@
 package com.jaemin.main.data.remote
 
+import com.jaemin.main.data.dto.response.GalleryResponse
+import com.jaemin.main.data.dto.response.PostsResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MainApi {
+    @GET("board/galleries")
+    fun getDefaultGalleries(@Query("gallery-type") galleryType: Int) : Single<List<GalleryResponse>>
+
+    @GET("board/posts")
+    fun getGalleryPosts(@Query("per-page") perPage : Int,@Query("page") page : Int,@Query("gallery-id") galleryId : Int) : Single<PostsResponse>
+
+    @GET("users/{username}")
+    fun getUserInfo(@Path("username") username : String) : Single<PostsResponse>
+
 }
