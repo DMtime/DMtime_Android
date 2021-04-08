c2dd63af34ce3830f8373aed42f3b0cd06f26baa
yujaemin <youjmen15@gmail.com>
2021-04-08 20:02:50 +0900

    [ADD] 의존성 역전을 위한 repository

---
+++ DMtime/main/src/main/java/com/jaemin/main/data/repository/MainRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.jaemin.main.data.repository
+
+import com.jaemin.main.data.dto.response.GalleryResponse
+import com.jaemin.main.data.dto.response.toEntity
+import com.jaemin.main.data.remote.MainApi
+import com.jaemin.main.domain.entity.DefaultGallery
+import com.jaemin.main.domain.repository.MainRepository
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainRepositoryImpl(private val mainApi: MainApi) : MainRepository {
+
+    override fun getDefaultGalleries(): Single<List<DefaultGallery>> {
+
+
+        val galleriesSingle : Single<List<GalleryResponse>> = mainApi.getDefaultGalleries(1)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+
+        val defaultGallery : Single<List<DefaultGallery>>  =Single.zip(galleriesSingle, galleriesSingle.flatMap {
+            (Observable.fromIterable(it).flatMap { gallery->
+                mainApi.getGalleryPosts(4, 0, gallery.id).toObservable()
+            }).toList()
+
+        },{gallery, posts->
+            val defaultGalleries = mutableListOf<DefaultGallery>()
+            for (i in gallery.indices){
+                defaultGalleries.add(DefaultGallery(gallery[i].id, gallery[i].name, posts[i].posts.map { it.toEntity() }))
+            }
+            defaultGalleries
+        })
+        return defaultGallery
+    }
+//    fun getGalleries()
+//
+//    fun getPostsByGalleryId
+}
