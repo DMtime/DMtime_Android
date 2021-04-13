162049d96edb03000fd0f2f3ed3054a1d589e302
yujaemin <youjmen15@gmail.com>
2021-04-13 10:41:21 +0900

    [ADD] 실패 view 함수 추가

--- DMtime/main/src/main/java/com/jaemin/main/presentation/MainFragment.kt
+++ DMtime/main/src/main/java/com/jaemin/main/presentation/MainFragment.kt
@@ -14,6 +14,7 @@ import com.jaemin.main.databinding.FragmentMainBinding
 import com.jaemin.main.domain.entity.DefaultGallery
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
+import splitties.toast.toast
 
 
 class MainFragment : BaseFragment<FragmentMainBinding>(), MainContract.View {
@@ -29,6 +30,10 @@ class MainFragment : BaseFragment<FragmentMainBinding>(), MainContract.View {
 
     override fun setDefaultGalleries(defaultGalleries: List<DefaultGallery>) {
         binding.defaultGalleriesRv.adapter = DefaultGalleriesAdapter(defaultGalleries)
+    }
+
+    override fun setDefaultGalleriesFailed() {
+        toast("글을 불러오지 못했어요")
     }
 
     override fun onPause() {
