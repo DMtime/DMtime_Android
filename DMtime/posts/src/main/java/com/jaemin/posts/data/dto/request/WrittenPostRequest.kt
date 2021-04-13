481bc71b192e6618ce06a5b0c793b283a1139866
yujaemin <youjmen15@gmail.com>
2021-04-13 10:24:28 +0900

    [MODIFY] main 일부 dto들을 posts 모듈로 이동한 것에 의한 dto 삭제와 생성

---
+++ DMtime/posts/src/main/java/com/jaemin/posts/data/dto/request/WrittenPostRequest.kt
@@ -0,0 +1,16 @@
+package com.jaemin.posts.data.dto.request
+
+import com.google.gson.annotations.SerializedName
+import com.jaemin.posts.domain.entity.WrittenPost
+
+data class WrittenPostRequest(
+    @SerializedName("is_anonymous")
+    val isAnonymous: Boolean,
+    @SerializedName("image_ids")
+    val imageIds: List<Int>,
+    val content: String,
+    val title: String
+)
+
+fun WrittenPost.toData() =
+    WrittenPostRequest(this.isAnonymous, this.imageIds, this.content, this.title)
