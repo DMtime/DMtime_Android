813f5e2777c7d536c764cb7f19207d64ec44532f
yujaemin <youjmen15@gmail.com>
2021-04-13 10:20:45 +0900

    [ADD] base usecase

---
+++ DMtime/base/src/main/java/com/jaemin/base/UseCase.kt
@@ -0,0 +1,24 @@
+package com.jaemin.base
+
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+
+abstract class UseCase<P, R> {
+    private val compositeDisposable = CompositeDisposable()
+    abstract fun build(data: P): Single<R>
+
+    fun execute(data: P, disposableSingleObserver: DisposableSingleObserver<R>) {
+        val observer = build(data)
+            .subscribeOn(SchedulerProvider.io())
+            .observeOn(SchedulerProvider.ui())
+            .subscribeWith(disposableSingleObserver)
+        compositeDisposable.add(observer)
+
+    }
+
+    fun dispose() {
+        compositeDisposable.dispose()
+    }
+
+}
