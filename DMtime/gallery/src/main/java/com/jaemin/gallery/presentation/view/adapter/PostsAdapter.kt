191c643f3046158eaf351c8491805c2a53104c3c
yujaemin <youjmen15@gmail.com>
2021-04-14 23:41:35 +0900

    [MODIFY] recyclerview item이름 xxx_item에서 item_xxx로 변경

--- DMtime/gallery/src/main/java/com/jaemin/gallery/presentation/view/adapter/PostsAdapter.kt
+++ DMtime/gallery/src/main/java/com/jaemin/gallery/presentation/view/adapter/PostsAdapter.kt
@@ -3,15 +3,15 @@ package com.jaemin.gallery.presentation.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.jaemin.gallery.databinding.PostItemBinding
+import com.jaemin.gallery.databinding.ItemPostBinding
 import com.jaemin.gallery.domain.entity.PostPreview
 import com.jaemin.gallery.presentation.presenter.GalleryPresenter
 
 class PostsAdapter(private val defaultPosts : MutableList<PostPreview> = mutableListOf(), private val galleryPresenter: GalleryPresenter) : RecyclerView.Adapter<PostsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = PostItemBinding.inflate(inflater, parent, false)
-        return PostsViewHolder(binding,galleryPresenter)
+        val binding = ItemPostBinding.inflate(inflater, parent, false)
+        return PostsViewHolder(binding, galleryPresenter)
     }
 
     override fun onBindViewHolder(holder: PostsViewHolder, position: Int) {
@@ -20,8 +20,12 @@ class PostsAdapter(private val defaultPosts : MutableList<PostPreview> = mutable
 
     override fun getItemCount(): Int = defaultPosts.size
 }
-class PostsViewHolder(private val binding: PostItemBinding, private val galleryPresenter: GalleryPresenter) : RecyclerView.ViewHolder(binding.root){
-    fun bind(post: PostPreview){
+
+class PostsViewHolder(
+    private val binding: ItemPostBinding,
+    private val galleryPresenter: GalleryPresenter
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(post: PostPreview) {
         binding.postLayout.setOnClickListener {
             galleryPresenter.onClickPost(adapterPosition)
         }
