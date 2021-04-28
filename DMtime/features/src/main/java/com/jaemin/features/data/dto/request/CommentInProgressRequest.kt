85aade20bfe509f8f9bf7c2f505dc927354b171b
yujaemin <youjmen15@gmail.com>
2021-04-28 23:28:05 +0900

    [MODIFY] 기능 변경으로 인한 entity, dto 수정

--- DMtime/features/src/main/java/com/jaemin/features/data/dto/request/CommentInProgressRequest.kt
+++ DMtime/features/src/main/java/com/jaemin/features/data/dto/request/CommentInProgressRequest.kt
@@ -1,12 +1,16 @@
-
 package com.jaemin.features.data.dto.request
 
 import com.google.gson.annotations.SerializedName
+import com.jaemin.features.domain.entity.CommentInProgress
 
 data class CommentInProgressRequest(
     @SerializedName("is_anonymous")
     val isAnonymous: Boolean,
     @SerializedName("upper_comment_id")
-    val upperCommentId: Int,
+    val upperCommentId: Int?,
     val content: String
 )
+
+fun CommentInProgress.toData() =
+    CommentInProgressRequest(this.isAnonymous,this.upperCommentId,this.content)
+
