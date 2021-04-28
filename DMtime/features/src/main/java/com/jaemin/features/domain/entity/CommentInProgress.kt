47c9eea2d7e265dae1922a96fb37ffb6d3cac917
yujaemin <youjmen15@gmail.com>
2021-04-28 23:32:36 +0900

    [MODIFY] 상위 댓글은 null일 수 있어 nullable로 수정

--- DMtime/features/src/main/java/com/jaemin/features/domain/entity/CommentInProgress.kt
+++ DMtime/features/src/main/java/com/jaemin/features/domain/entity/CommentInProgress.kt
@@ -2,6 +2,6 @@ package com.jaemin.features.domain.entity
 
 data class CommentInProgress(
     val isAnonymous: Boolean,
-    val upperCommentId: Int,
+    val upperCommentId: Int?,
     val content: String
 )
