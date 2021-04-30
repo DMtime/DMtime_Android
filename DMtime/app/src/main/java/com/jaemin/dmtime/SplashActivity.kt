dcfec60193f629bff4b01a3dd3b126016c578c98
yujaemin <youjmen15@gmail.com>
2021-04-30 10:01:26 +0900

    [MODIFY] 로그인 모듈 삭제로 인한 import

--- DMtime/app/src/main/java/com/jaemin/dmtime/SplashActivity.kt
+++ DMtime/app/src/main/java/com/jaemin/dmtime/SplashActivity.kt
@@ -2,14 +2,14 @@ package com.jaemin.dmtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jaemin.login.LoginFragment
+import com.jaemin.features.presentation.login.view.LoginFragment
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main_layout,LoginFragment())
+            .replace(R.id.main_layout, LoginFragment())
                 .addToBackStack(null)
                 .commit()
     }
