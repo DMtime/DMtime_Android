789c2390e207ababc3571e1281c015598a3e3d2f
yujaemin <youjmen15@gmail.com>
2021-05-09 14:52:49 +0900

    [ADD] 유저가 작성, 좋아요, 댓글 작성한 글 api

--- DMtime/features/src/main/java/com/jaemin/features/data/remote/PostsApi.kt
+++ DMtime/features/src/main/java/com/jaemin/features/data/remote/PostsApi.kt
@@ -14,4 +14,37 @@ interface PostsApi {
         @Query("gallery-id") galleryId: String
     ): Single<PostsResponse>
 
+    @GET("board/posts")
+    fun getUserPosts(
+        @Query("per-page") perPage: Int,
+        @Query("page") page: Int,
+        @Query("username") username: String
+    ): Single<PostsResponse>
+
+    @GET("users/{username}/posts-wrote-comment")
+    fun getUserWroteCommentPosts(
+        @Path("username") username : String,
+        @Query("per-page") perPage: Int,
+        @Query("page") page: Int
+    ): Single<PostsResponse>
+
+    @GET("users/{username}/liked-posts")
+    fun getUserLikedPosts(
+        @Path("username") username : String,
+        @Query("per-page") perPage: Int,
+        @Query("page") page: Int
+    ): Single<PostsResponse>
+
+    @GET("me/posts-wrote-comment")
+    fun getMyWroteCommentPosts(
+        @Query("per-page") perPage: Int,
+        @Query("page") page: Int
+    ): Single<PostsResponse>
+
+    @GET("me/liked-posts")
+    fun getMyLikedPosts(
+        @Query("per-page") perPage: Int,
+        @Query("page") page: Int
+    ): Single<PostsResponse>
+
 }
