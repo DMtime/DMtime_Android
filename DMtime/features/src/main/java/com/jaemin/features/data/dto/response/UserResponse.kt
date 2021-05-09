86115769ca0689df0f013852f3e62a9b5f7e4558
yujaemin <youjmen15@gmail.com>
2021-05-10 01:16:10 +0900

    [MODIFY] dto property 직렬화

--- DMtime/features/src/main/java/com/jaemin/features/data/dto/response/UserResponse.kt
+++ DMtime/features/src/main/java/com/jaemin/features/data/dto/response/UserResponse.kt
@@ -1,9 +1,11 @@
 package com.jaemin.features.data.dto.response
 
+import com.google.gson.annotations.SerializedName
 import com.jaemin.features.domain.entity.User
 
 data class UserResponse(
     val explain : String?,
+    @SerializedName("profile_image")
     val profileImage : String?,
     val username : String
 )
