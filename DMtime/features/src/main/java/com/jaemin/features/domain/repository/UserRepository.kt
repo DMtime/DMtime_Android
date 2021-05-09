cce85ca5286da033efecabc0cc6cdf4c9e3171ac
yujaemin <youjmen15@gmail.com>
2021-05-10 01:06:50 +0900

    [ADD] 유저 정보 수정 구현

--- DMtime/features/src/main/java/com/jaemin/features/domain/repository/UserRepository.kt
+++ DMtime/features/src/main/java/com/jaemin/features/domain/repository/UserRepository.kt
@@ -1,8 +1,13 @@
 package com.jaemin.features.domain.repository
 
 import com.jaemin.features.domain.entity.User
+import com.jaemin.features.domain.requestmodel.EditedProfile
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
 interface UserRepository {
     fun getUserInfo(username : String) : Single<User>
+
+    fun changeUserInfo(currentUser: User, editedUser: EditedProfile) : Completable
+
 }
